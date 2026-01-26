96f500950da53c141ee93df281fcbd362e9384f0
Major <major@major.holdings>
2026-01-26 16:20:45 -0500

    seeder not working in prod

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'first_name' => fake()->firstName(),
-            'last_name' => fake()->lastName(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),

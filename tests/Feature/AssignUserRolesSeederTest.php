23c37f3b274989e7278d5c046ba03347e028568d
Major <major@major.holdings>
2026-02-09 14:35:29 -0500

    refresh database test issues

--- tests/Feature/AssignUserRolesSeederTest.php
+++ tests/Feature/AssignUserRolesSeederTest.php
@@ -3,9 +3,6 @@
 use App\Models\User;
 use Database\Seeders\AssignUserRolesSeeder;
 use Database\Seeders\PermissionsSeeder;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-
-uses(RefreshDatabase::class);
 
 beforeEach(function () {
     // Run PermissionsSeeder to ensure roles exist
